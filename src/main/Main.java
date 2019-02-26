2e8d29535a05632d48690488c7047cbdf9abe900
Alisson Muhammad <alisson.muhammad@hotmail.com>
2019-02-26 10:50:49 -0300

    Chess Board Created

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,14 @@
 package main;
 
-import boardGame.Board;
+import chess.ChessMatch;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 
 }
